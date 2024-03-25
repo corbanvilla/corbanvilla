8992ac1475d4d5a024ca89390101f5ce78a789de
corbanvilla <corbanvilla@gmail.com>
2024-03-25 21:02:21 +0400

    responsive design

--- site-v2/app/layout.tsx
+++ site-v2/app/layout.tsx
@@ -42,37 +42,53 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
 
+  const Icons = ({ className }: { className?: string }) => {
+    return (
+      <div className={`${className || ""}`}>
+        <div className="flex flex-row gap-4 justify-end md:justify-start">
+          <Icon src="/icons/github.svg" alt="GitHub" href="https://github.com/corbanvilla"/>
+          <Icon src="/icons/twitter.svg" alt="Twitter" href="https://twitter.com/Animcogn"/>
+        </div>
+      </div>
+    )
+  }
+
   return (
     <html lang="en" className={`${sans.variable} ${mono.variable}`}>
       <body>
-        <div className="mt-32 flex justify-center gap-8">
+        <div className="m-4 md:mt-32 flex flex-col md:flex-row justify-center gap-4 md:gap-8">
           {/* Left sidebar */}
-          <div className="flex flex-col gap-6">
-            {/* Image */}
-            <div className="w-64 h-64 relative">
-              <Image src="/corban.jpg" fill alt="Corban" />
+          <div className="flex flex-col gap-3 md:gap-6">
+            <div className="flex flex-row md:flex-col items-center md:items-start gap-4 md:gap-6 md:-mb-2">
+              {/* Image */}
+              <Link href="/">
+                <div className="w-12 h-12 md:w-64 md:h-64 relative">
+                  <Image src="/corban.jpg" fill alt="Corban" />
+                </div>
+              </Link>
+              {/* Title */}
+              <Link href="/">
+                <h1 className="text-2xl md:text-3xl font-sans font-medium">Corban Villa</h1>
+              </Link>
+              {/* Social Media Icons */}
+              <Icons className="md:hidden flex-auto"/>
+
             </div>
-            {/* Title */}
-            <Link href="/">
-              <h1 className="text-3xl font-sans font-medium">Corban Villa</h1>
-            </Link>
             {/* Submenus */}
-            <div className="flex flex-col gap-3 font-mono">
-              <NavigationLink href="/" title="home" notarget={true} />
+            <div className="flex flex-row md:flex-col gap-3 font-mono justify-between md:justify-normal w-full">
+              <NavigationLink href="/" title="home" notarget={true} className="hidden md:block" />
               <NavigationLink href="/publications" title="publications" notarget={true} />
               <NavigationLink href="https://github.com/corbanvilla/corbanvilla/blob/main/CorbanVillaResume.pdf" title="resume" />
               <NavigationLink href="mailto:hello@corbanvilla.com" title="contact" />
             </div>
             {/* Icons */}
-            <div className="flex flex-row gap-4">
-              <Icon src="/icons/github.svg" alt="GitHub" href="https://github.com/corbanvilla"/>
-              <Icon src="/icons/twitter.svg" alt="Twitter" href="https://twitter.com/Animcogn"/>
-            </div>
+            <Icons className="hidden md:block"/>
           </div>
           {/* Single Pixel Line */}
-          <div className="w-px h-128 bg-gray-200" />
+          <div className="w-px h-128 bg-gray-200 hidden md:block" />
+          <div className="w-screen h-px bg-gray-200 block md:hidden" />
           {/* Right content */}
-          <div className="w-[32rem]">
+          <div className="md:w-[32rem]">
             {children}
           </div>
         </div>
--- site-v2/components/NavigationLink.tsx
+++ site-v2/components/NavigationLink.tsx
@@ -2,12 +2,12 @@
 import { usePathname } from 'next/navigation';
 import Link from "next/link";
 
-export default function NavigationLink({ href, title, notarget }: { href: string, title: string, notarget?: boolean }) {
+export default function NavigationLink({ href, title, notarget, className }: { href: string, title: string, notarget?: boolean, className?: string }) {
   const pathname = usePathname();
   const target = notarget ? "" : "_blank";
 
   return (
-    <Link href={href} className={pathname === href ? "text-blue-500" : ""} target={target}>
+    <Link href={href} className={`${className || ""} ${pathname === href ? "md:text-blue-500 font-semibold md:font-normal" : ""} underline md:no-underline`} target={target}>
       {title}
     </Link>
   );
