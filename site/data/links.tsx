5bf018007c535e612d3a07fed7d909ba477a6502
corbanvilla <corbanvilla@gmail.com>
2024-12-27 16:26:24 -0700

    remove old site

--- site/data/links.tsx
+++
@@ -1,6 +0,0 @@
-export default {
-    "email": "mailto:hello@corbanvilla.com",
-    "linkedin": "https://www.linkedin.com/in/corbanvilla/",
-    "twitter": "https://twitter.com/animcogn",
-    "github": "https://github.com/corbanvilla"
-}
