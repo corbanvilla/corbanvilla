98d7409ef21e631c38cbbc467857ae168ff40af7
corbanvilla <corbanvilla@gmail.com>
2025-01-28 15:34:02 +0400

    lowercase

--- site/app/(main)/resources/route.tsx
+++ site/app/(main)/resources/route.tsx
@@ -4,7 +4,7 @@ import resources from "../../../resources.json" assert { type: "json" };
 
 export function GET(request: NextRequest) {
     const searchParams = request.nextUrl.searchParams
-    const path = searchParams.get('path') || "";
+    const path = (searchParams.get('path') || "").toLowerCase();
     const resourcesMap = resources as Record<string, string>;
     const destinationPath = resourcesMap[path] ? resourcesMap[path] : resourcesMap["default"];
     return redirect(destinationPath);
