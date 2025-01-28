4e31c1bdb99cc5879db44f137a4878782a982ed6
corbanvilla <corbanvilla@gmail.com>
2025-01-28 15:19:55 +0400

    redirect from resources

---
+++ site/app/(main)/resources/route.tsx
@@ -0,0 +1,11 @@
+import { redirect } from "next/navigation";
+import { type NextRequest } from 'next/server'
+import resources from "../../../resources.json" assert { type: "json" };
+
+export function GET(request: NextRequest) {
+    const searchParams = request.nextUrl.searchParams
+    const path = searchParams.get('path') || "";
+    const resourcesMap = resources as Record<string, string>;
+    const destinationPath = resourcesMap[path] ? resourcesMap[path] : resourcesMap["default"];
+    return redirect(destinationPath);
+}
