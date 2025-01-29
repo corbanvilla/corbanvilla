b49695c08975710c549a381acda90b635828fe90
corbanvilla <corbanvilla@gmail.com>
2025-01-29 11:59:51 +0400

    fix redirects

--- site/app/resources/route.tsx
+++ site/app/resources/route.tsx
@@ -1,11 +1,20 @@
 import { redirect } from "next/navigation";
 import { type NextRequest } from 'next/server'
-import resources from "../../../resources.json" assert { type: "json" };
+import resources from "../../resources.json" assert { type: "json" };
+
+const resourcesMap = resources as Record<string, string>;
+
+Object.keys(resourcesMap).forEach(key => {
+    const lowerKey = key.toLowerCase();
+    if (key !== lowerKey) {
+        resourcesMap[lowerKey] = resourcesMap[key];
+        delete resourcesMap[key];
+    }
+});
 
 export function GET(request: NextRequest) {
     const searchParams = request.nextUrl.searchParams
     const path = (searchParams.get('path') || "").toLowerCase();
-    const resourcesMap = resources as Record<string, string>;
     const destinationPath = resourcesMap[path] ? resourcesMap[path] : resourcesMap["default"];
     return redirect(destinationPath);
 }
