19af95a4bab0948c0016aa3eb520bbfce6e60448
corbanvilla <corbanvilla@gmail.com>
2026-01-10 20:35:04 +0400

    enable static export

--- site/next.config.mjs
+++ site/next.config.mjs
@@ -1,22 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-import redirectsMap from './redirects.json' with {type: 'json'};
-import rewritesMap from './rewrites.json' with {type: 'json'};
-
 const nextConfig = {
-    redirects: async () => {
-        return Object.entries(redirectsMap).map(([source, destination]) => ({
-            source: source,
-            destination: destination,
-            permanent: false,
-        }));
-    },
-    rewrites: async () => {
-        return Object.entries(rewritesMap).map(([source, destination]) => ({
-            source: source,
-            destination: destination,
-        }));
-    }
+    output: 'export',
 };
 
 export default nextConfig;
